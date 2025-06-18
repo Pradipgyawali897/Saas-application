03e1410f7f1ab63616ca9e82f7c2dd41685def72
Pradip gyawali <gyawalipradip846@gmail.com>
2025-06-18 07:40:11 +0545

    extended the configurations

--- src/cfe_home/settings.py
+++ src/cfe_home/settings.py
@@ -119,7 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR=BASE_DIR/'staticfiles'
 
+STATICFILES_DIRS=[
+    STATICFILES_BASE_DIR
+]
+STATIC_ROOT=BASE_DIR.parent/'local-cdn'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
