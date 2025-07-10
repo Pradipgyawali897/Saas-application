a6c3def0c8c73fdd0cc71e0f221ec215d10b9a73
pradip gyawali <gyawalipradip846@gmail.com>
2025-07-10 12:27:35 +0545

    fix: update Stripe customer email on django-allauth email change to preserve subscription(#3)

--- src/constumers/models.py
+++ src/constumers/models.py
@@ -2,12 +2,47 @@ from django.db import models
 from django.contrib.auth.models import User
 import helpers
 import helpers.billing
-class Constumer(models.Model):
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
-    stripe_id=models.CharField(max_length=120,null=True,blank=True)
+from allauth.account.signals import user_signed_up as allauth_user_signed_up,email_confirmed as allauth_email_confirmed
 
+class Constumer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stripe_id = models.CharField(max_length=120, null=True, blank=True)
+    init_email=models.EmailField(blank=True,null=True)
+    init_email_confirmed=models.BooleanField(default=False)
     def __str__(self):
         return self.user.username
-    def save(self,*args, **kwargs):
-        stripe_id=helpers.billing.create_consumer(raw=True)
+
+    def save(self, *args, **kwargs):
+        if self.stripe_id:
+            if self.init_email_confirmed and self.init_email:
+                email = self.init_email
+                if email:
+                    response = helpers.billing.create_customer(
+                        name=self.user.username,
+                        email=email,
+                        raw=False,
+                        metadata={'user_id':self.user.id,'uesrname':self.user.username},
+                    )
+                    self.stripe_id = response.id
         return super().save(*args, **kwargs)
+def allauth_user_signed_up_handler(request,user,*args, **kwargs):
+    email=user.email
+    Constumer.objects.create(
+        user=user,
+        init_email=email,
+        init_email_confirmed=True,
+
+    )
+
+allauth_user_signed_up.connect(allauth_user_signed_up_handler)
+
+def allauth_email_confirmed_handler(request,user,*args, **kwargs):
+    qs=Constumer.objects.filter(
+        user=user,
+        init_email_confirmed=True,
+    )
+    for obj in qs:
+        obj.init_email_confirmed=True
+        obj.save()
+
+allauth_email_confirmed.connect(allauth_email_confirmed_handler)
--- src/helpers/billing.py
+++ src/helpers/billing.py
@@ -1,18 +1,22 @@
-# Set your secret key. Remember to switch to your live secret key in production.
-# See your keys here: https://dashboard.stripe.com/apikeys
-from decouple import config
 import stripe
-SECRET_API_KEY=config('API_KEY')
-DJANGO_DEBUG=config('DEBUG',default=False,cast=bool)
+from decouple import config
+
+SECRET_API_KEY = config('API_KEY')
+DJANGO_DEBUG = config('DEBUG', default=False, cast=bool)
+
 if 'sk_test' in SECRET_API_KEY and not DJANGO_DEBUG:
     raise ValueError("Invalid stripe key")
+
 stripe.api_key = SECRET_API_KEY
-def create_consumer(raw=True):
-    customer = stripe.Customer.create(
-    name="",
-    email="",
-    )
-    if raw:
-        return customer
-    stripe_id=customer.id
-    return stripe_id
+
+def create_customer(name="", email="", metadata={}, raw=True):
+    try:
+        response = stripe.Customer.create(
+            name=name,
+            email=email,
+            metadata=metadata,
+        )
+        return response if raw else response.id
+    except Exception as e:
+        print("Stripe Error:", e)
+        raise
